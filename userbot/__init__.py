ed5e5a3570b3fd1bd25f5a003ee27c971748b139
jefa2231 <39442318+jefa2231@users.noreply.github.com>
2020-04-05 22:44:39 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -75,7 +75,7 @@ HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/jefa2231/Auto.git")
+    "https://github.com/jefa2231/nova.git")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
